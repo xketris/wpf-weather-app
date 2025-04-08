ba640e84db1d7eee42e51bac12dfdb0188073e44
szymon-skalmierski <xketris@gmail.com>
2025-04-08 17:32:52 +0200

    weather service

--- WeatherService.cs
+++ WeatherService.cs
@@ -1,17 +1,15 @@
-﻿using Newtonsoft.Json; // Potrzebne do deserializacji JSON
+﻿using Newtonsoft.Json;
 using System;
 using System.Diagnostics;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 
-// Serwis pogodowy pobierający dane z API OpenWeatherMap
 public class WeatherService
 {
     private const string ApiKey = "0608319f1864642730796423be649686";
     private const string BaseUrl = "http://api.openweathermap.org/data/2.5/weather";
 
-    // Obiekt HttpClient do wykonywania żądań HTTP
     private readonly HttpClient _client;
 
     public WeatherService()
@@ -19,26 +17,17 @@ public class WeatherService
         _client = new HttpClient();
     }
 
-    // Metoda asynchroniczna pobierająca dane pogodowe dla podanego miasta
-    // async oznacza, że metoda może działać asynchronicznie.
-    // await oznacza, że program nie blokuje wykonywania kodu, tylko czeka na odpowiedź z API.
-    // Task<T> jest typem zwracanym przez metody asynchroniczne – reprezentuje obiekt, który będzie miał wartość w przyszłości.
-    // Dzięki async/await aplikacja nie blokuje głównego wątku, co jest kluczowe dla responsywności
     public async Task<WeatherData?> GetWeatherAsync(string city)
     {
         try
         {
-            // Wysyłamy asynchroniczne żądanie GET do API
             //HttpResponseMessage response = await _client.GetAsync($"{BaseUrl}?q={city}&lang=pl&appid={ApiKey}&units=metric");
             HttpResponseMessage response = await _client.GetAsync($"{BaseUrl}?q={city}&appid={ApiKey}&units=metric");
 
-            // Sprawdzenie, czy odpowiedź jest poprawna (status 200 OK)
             response.EnsureSuccessStatusCode();
 
-            // Pobranie treści odpowiedzi jako string
             string responseBody = await response.Content.ReadAsStringAsync();
 
-            // Deserializacja JSON do obiektu WeatherData
             WeatherData? weatherData = JsonConvert.DeserializeObject<WeatherData>(responseBody);
 
             return weatherData;
@@ -54,7 +43,6 @@ public class WeatherService
 }
 
 
-// Klasa reprezentująca strukturę danych pogodowych (zgodnie z formatem JSON zwracanym przez API)
 public class WeatherData
 {
     [JsonProperty("main")]
@@ -67,7 +55,7 @@ public class WeatherData
 public class MainData
 {
     [JsonProperty("temp")]
-    public float Temperature { get; set; } // Lepsza nazwa niż "Temp"
+    public float Temperature { get; set; }
 
     [JsonProperty("humidity")]
     public float Humidity { get; set; }
